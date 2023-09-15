b8ed88834f96fed96c6be1b08648169b932bfbd4
CoyuyoUTN <herrero_gonza@hotmail.com>
2023-09-15 15:13:21 -0300

    fixed problem in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,46 +1,69 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 const Nav = ({ t }) => {
-  // Selecciona todos los enlaces que deben cerrar el navbar
-  const navLinks = document.querySelectorAll(".navbar a");
+  const [isActive, setIsActive] = useState(false);
+  const myElementRef = useRef(null);
 
-  // Agrega un evento de clic a cada enlace
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      // Desactiva la casilla de verificación del menú
-      const checkbox = document.getElementById("active");
-      checkbox.checked = false;
-    });
-  });
+  const handleCheckboxChange = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleLinkClick = () => {
+    if (myElementRef.current) {
+      myElementRef.checked = "false";
+    }
+    setIsActive(false);
+  };
+
   return (
     <div className="navbar">
-      <input type="checkbox" id="active" />
-      <label for="active" class="menu-btn">
+      <input
+        type="checkbox"
+        id="active"
+        checked={isActive}
+        onChange={handleCheckboxChange}
+        ref={myElementRef}
+      />
+      <label htmlFor="active" className="menu-btn">
         <span></span>
       </label>
-      <label for="active" class="close"></label>
-      <div class="wrapper">
+      <label htmlFor="active" className="close"></label>
+      <div className="wrapper">
         <ul>
           <li>
-            <a href="#home-nav">{t("nav")}</a>
+            <a href="#home-nav" onClick={handleLinkClick}>
+              {t("nav")}
+            </a>
           </li>
           <li>
-            <a href="#about-nav">{t("nav1")}</a>
+            <a href="#about-nav" onClick={handleLinkClick}>
+              {t("nav1")}
+            </a>
           </li>
           <li>
-            <a href="#projects-nav">{t("nav2")}</a>
+            <a href="#projects-nav" onClick={handleLinkClick}>
+              {t("nav2")}
+            </a>
           </li>
           <li>
-            <a href="#education-nav">{t("nav3")}</a>
+            <a href="#education-nav" onClick={handleLinkClick}>
+              {t("nav3")}
+            </a>
           </li>
           <li>
-            <a href="#awards-nav">{t("nav4")}</a>
+            <a href="#awards-nav" onClick={handleLinkClick}>
+              {t("nav4")}
+            </a>
           </li>
           <li>
-            <a href="#supervised-nav">{t("nav5")}</a>
+            <a href="#supervised-nav" onClick={handleLinkClick}>
+              {t("nav5")}
+            </a>
           </li>
           <li>
-            <a href="#contact-nav">{t("nav6")}</a>
+            <a href="#contact-nav" onClick={handleLinkClick}>
+              {t("nav6")}
+            </a>
           </li>
         </ul>
       </div>
